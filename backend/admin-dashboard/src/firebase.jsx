d52f7419ac27a855230b8a76689cc00e3ef874cd
Atul Jha <69789941+Atul240202@users.noreply.github.com>
2024-11-30 23:25:49 +0530

    Update firebase.jsx

--- backend/admin-dashboard/src/firebase.jsx
+++ backend/admin-dashboard/src/firebase.jsx
@@ -34,14 +34,14 @@ import {
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: 'AIzaSyAP5PHGZzfwWmpokGKo3CZOJVLo-r0w4Zg',
-  authDomain: 'go-productions.firebaseapp.com',
-  databaseURL: 'https://go-productions-default-rtdb.firebaseio.com',
-  projectId: 'go-productions',
-  storageBucket: 'go-productions.firebasestorage.app',
-  messagingSenderId: '518642601161',
-  appId: '1:518642601161:web:8bae0f4dcd0f5ec76dd118',
-  measurementId: 'G-WV6RVR1TZV',
+  apiKey: '',
+  authDomain: '',
+  databaseURL: '',
+  projectId: '',
+  storageBucket: '',
+  messagingSenderId: '',
+  appId: '',
+  measurementId: '',
 };
 
 // Initialize Firebase
